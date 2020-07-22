19451f39452bab6c2d2c5ada1c6bf0b160cea797
sofia_mikolaiv <sofia.mikolaiv@gmail.com>
2020-07-22 22:35:01 +0300

    Add splash

--- app/src/main/java/com/example/spacex_app/presentation/fragment/SplashFragment.kt
+++ app/src/main/java/com/example/spacex_app/presentation/fragment/SplashFragment.kt
@@ -1,11 +1,32 @@
 package com.example.spacex_app.presentation.fragment
 
+import android.os.Bundle
+import androidx.lifecycle.Observer
 import com.example.spacex_app.R
 import com.example.spacex_app.presentation.viewmodel.SplashViewModel
+import com.example.spacex_app.utiles.showSimpleErrorDialog
 import org.koin.android.ext.android.inject
 
 class SplashFragment : BaseFragment<SplashViewModel>() {
+
     override val viewModel by inject<SplashViewModel>()
 
     override fun getLayoutRes() = R.layout.fragment_splash
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        viewModel.openHome()
+        observeData()
+    }
+
+    private fun observeData() {
+        viewModel.splashLiveData.observe(viewLifecycleOwner, Observer {
+            navigateSafe(R.id.action_splash_fragment_to_home_fragment)
+        })
+
+        viewModel.errorMessageLiveData.observe(viewLifecycleOwner, Observer { exceptionMessage ->
+            showSimpleErrorDialog(context, null, exceptionMessage, null, null)
+        })
+    }
 }
--- app/src/main/java/com/example/spacex_app/presentation/viewmodel/SplashViewModel.kt
+++ app/src/main/java/com/example/spacex_app/presentation/viewmodel/SplashViewModel.kt
@@ -1,4 +1,25 @@
 package com.example.spacex_app.presentation.viewmodel
 
-class SplashViewModel: BaseViewModel() {
+import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashViewModel : BaseViewModel() {
+
+    companion object {
+        private const val DELAY = 2000L
+    }
+
+    val splashLiveData = MutableLiveData<Unit>()
+
+    fun openHome() = CoroutineScope(Dispatchers.Main).launch {
+        try {
+            delay(DELAY)
+            splashLiveData.value = Unit
+        } catch (e: Exception) {
+            errorMessageLiveData.postValue(e.message)
+        }
+    }
 }
